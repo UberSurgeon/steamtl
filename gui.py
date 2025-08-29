694707746dfa3431f87446d513e132b1cca58af7
UberSurgeon <yepplaz@gmail.com>
2025-08-29 16:20:39 +0300

    change gui layout

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow,
-                             QWidget, QVBoxLayout, QPushButton)
+                             QWidget, QVBoxLayout, QToolButton)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, Qt
 import os
 
 
@@ -10,26 +10,30 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("steamtl")
-        self.setGeometry(700, 300, 500, 500)
+        self.setGeometry(700, 300, 0, 0)
         self.initUI()
         self.setWindowIcon(QIcon('./img/steamtl'))
 
     def initUI(self):
+
         central__widgit = QWidget()
         self.setCentralWidget(central__widgit)
 
-        button1 = QPushButton('Limbus', self)
-        button2 = QPushButton('Zomboid', self)
+        button1 = QToolButton()
+        button2 = QToolButton()
+        button1.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
+        button2.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 
         button1.setStyleSheet('font-size: 30px;')
         button1.setIcon(QIcon('./img/limbus'))
-        button1.setIconSize(QSize(500, 250))
-        button1.setFixedSize(500, 250)
+        button1.setIconSize(QSize(250, 250))
+        button1.setText('Limbus')
         button1.clicked.connect(lambda: self.on_click('limbus_company'))
+
         button2.setStyleSheet('font-size: 30px;')
-        button2.setIcon(QIcon('./img/limbus'))
-        button2.setIconSize(QSize(500, 250))
-        button2.setFixedSize(500, 250)
+        button2.setIcon(QIcon('./img/zomboid'))
+        button2.setIconSize(QSize(250, 250))
+        button2.setText('Zomboid')
         button2.clicked.connect(lambda: self.on_click('project_zomboid'))
 
         vbox = QVBoxLayout()
@@ -42,11 +46,13 @@ class MainWindow(QMainWindow):
     def on_click(self, cmd_name):
         os.system(f'{cmd_name}.cmd')
 
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
